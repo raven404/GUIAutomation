e63433a567592945ee0c5e6c24c6ee386614d278
raven <32195921+raven404@users.noreply.github.com>
2020-05-10 23:53:47 -0700

    keyboard controls tested

---
+++ controlingkeys.py
@@ -0,0 +1,12 @@
+import pyautogui
+
+pyautogui.click(100,100)
+pyautogui.typewrite("hello folks")
+
+pyautogui.click(100,100)
+
+pyautogui.typewrite("hello folks" interval=2)
+
+pyautogui.press('f5')
+
+pyautogui.hotkey('alt','f4')
